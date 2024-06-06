13eb875fd4c8412b669319220dedd0fc7b307744
Tauhid hossen <tauhidhossen04@gmail.com>
2024-06-06 23:55:23 +0600

    Admin can change user to Admin

--- index.js
+++ index.js
@@ -53,6 +53,20 @@ async function run() {
 
 
 
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id =req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result =await userCollection.updateOne(filter,updatedDoc)
+      res.send(result);
+    })
+
+
+
     app.post('/products',async(req,res)=>{
 
         const newProduct =req.body;
