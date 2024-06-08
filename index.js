19cb779cbc47ac97212160b21b2c60b0e54f2a9e
Tauhid hossen <tauhidhossen04@gmail.com>
2024-06-09 00:25:24 +0600

    meke moderator

--- index.js
+++ index.js
@@ -94,6 +94,17 @@ const verifyToken =(req,res,next) =>{
       const result =await userCollection.updateOne(filter,updatedDoc)
       res.send(result);
     })
+    app.patch('/users/moderator/:id',async(req,res)=>{
+      const id =req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set: {
+          role: 'moderator'
+        }
+      }
+      const result =await userCollection.updateOne(filter,updatedDoc)
+      res.send(result);
+    })
 
 
 
